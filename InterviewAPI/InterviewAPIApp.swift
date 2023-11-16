f78d22b9a8bb16a454e88ff5acb8c403a15871dd
Asher McConnell <asherryan7@gmail.com>
2023-11-16 06:23:01 -0700

    Initial Commit

---
+++ InterviewAPI/InterviewAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewAPIApp.swift
+//  InterviewAPI
+//
+//  Created by Asher McConnell on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct InterviewAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
